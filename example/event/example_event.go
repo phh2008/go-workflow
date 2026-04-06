702b41338a1d2cc891f02b2c18ed67c9886f8d6e
penghuihui <peng.huihui@163.com>
2026-04-07 00:03:32 +0800

    style: example_event 方法参数命名统一为小驼峰

--- example/event/example_event.go
+++ example/event/example_event.go
@@ -33,31 +33,31 @@ func (e *MyEvent) getProcessNameByInstID(ctx context.Context, instID int) (strin
 }
 
 // OnEnd 节点结束事件
-func (e *MyEvent) OnEnd(ctx context.Context, ProcessInstanceID int, CurrentNode *model.Node, PrevNode model.Node) error {
-	processName, err := e.getProcessNameByInstID(ctx, ProcessInstanceID)
+func (e *MyEvent) OnEnd(ctx context.Context, instID int, currentNode *model.Node, prevNode model.Node) error {
+	processName, err := e.getProcessNameByInstID(ctx, instID)
 	if err != nil {
 		return err
 	}
-	slog.Info("节点结束", "流程", processName, "节点", CurrentNode.NodeName)
+	slog.Info("节点结束", "流程", processName, "节点", currentNode.NodeName)
 	return nil
 }
 
 // OnNotify 通知
-func (e *MyEvent) OnNotify(ctx context.Context, ProcessInstanceID int, CurrentNode *model.Node, PrevNode model.Node) error {
-	processName, err := e.getProcessNameByInstID(ctx, ProcessInstanceID)
+func (e *MyEvent) OnNotify(ctx context.Context, instID int, currentNode *model.Node, prevNode model.Node) error {
+	processName, err := e.getProcessNameByInstID(ctx, instID)
 	if err != nil {
 		return err
 	}
-	slog.Info("通知节点中对应人员", "流程", processName, "节点", CurrentNode.NodeName)
-	if CurrentNode.NodeType == model.EndNode {
+	slog.Info("通知节点中对应人员", "流程", processName, "节点", currentNode.NodeName)
+	if currentNode.NodeType == model.EndNode {
 		slog.Info("流程结束", "流程", processName)
-		variables, err := e.eng.ResolveVariables(ctx, model.ResolveVariablesParams{InstanceID: ProcessInstanceID, Variables: []string{"$starter"}})
+		variables, err := e.eng.ResolveVariables(ctx, model.ResolveVariablesParams{InstanceID: instID, Variables: []string{"$starter"}})
 		if err != nil {
 			return err
 		}
 		slog.Info("通知流程创建人流程已完成", "创建人", variables["$starter"], "流程", processName)
 	} else {
-		for _, user := range CurrentNode.UserIDs {
+		for _, user := range currentNode.UserIDs {
 			slog.Info("通知用户抓紧去处理", "用户", user)
 		}
 	}
@@ -65,16 +65,15 @@ func (e *MyEvent) OnNotify(ctx context.Context, ProcessInstanceID int, CurrentNo
 }
 
 // OnResolveRoles 解析角色
-func (e *MyEvent) OnResolveRoles(ctx context.Context, ProcessInstanceID int, CurrentNode *model.Node, PrevNode model.Node) error {
-	processName, err := e.getProcessNameByInstID(ctx, ProcessInstanceID)
+func (e *MyEvent) OnResolveRoles(ctx context.Context, instID int, currentNode *model.Node, prevNode model.Node) error {
+	processName, err := e.getProcessNameByInstID(ctx, instID)
 	if err != nil {
 		return err
 	}
-	slog.Info("开始解析角色", "流程", processName, "节点", CurrentNode.NodeName)
-	// 把用户库中对应角色的用户全部放到CurrentNode.UserIDs中去
-	for _, role := range CurrentNode.Roles {
+	slog.Info("开始解析角色", "流程", processName, "节点", currentNode.NodeName)
+	for _, role := range currentNode.Roles {
 		if users, ok := RoleUser[role]; ok {
-			CurrentNode.UserIDs = append(CurrentNode.UserIDs, users...)
+			currentNode.UserIDs = append(currentNode.UserIDs, users...)
 		}
 	}
 	return nil
@@ -83,8 +82,8 @@ func (e *MyEvent) OnResolveRoles(ctx context.Context, ProcessInstanceID int, Cur
 // OnTaskForceNodePass 任务事件
 // 在示例流程中，"副总审批"是一个会签节点，需要3个副总全部通过，节点才算通过
 // 现在通过任务事件改变会签通过人数，设为只要2人通过，即算通过
-func (e *MyEvent) OnTaskForceNodePass(ctx context.Context, TaskID int, CurrentNode *model.Node, PrevNode model.Node) error {
-	taskInfo, err := e.eng.GetTaskInfo(ctx, TaskID)
+func (e *MyEvent) OnTaskForceNodePass(ctx context.Context, taskID int, currentNode *model.Node, prevNode model.Node) error {
+	taskInfo, err := e.eng.GetTaskInfo(ctx, taskID)
 	if err != nil {
 		return err
 	}
@@ -93,7 +92,7 @@ func (e *MyEvent) OnTaskForceNodePass(ctx context.Context, TaskID int, CurrentNo
 	if err != nil {
 		return err
 	}
-	slog.Info("任务结束事件", "流程", processName, "节点", CurrentNode.NodeName, "任务ID", taskInfo.TaskID)
+	slog.Info("任务结束事件", "流程", processName, "节点", currentNode.NodeName, "任务ID", taskInfo.TaskID)
 
 	// 获取节点审批状态：总任务数、通过数、驳回数
 	type nodeStatus struct {
@@ -125,12 +124,12 @@ func (e *MyEvent) OnTaskForceNodePass(ctx context.Context, TaskID int, CurrentNo
 }
 
 // OnRevoke 撤销事件
-func (e *MyEvent) OnRevoke(ctx context.Context, ProcessInstanceID int, RevokeUserID string) error {
-	processName, err := e.getProcessNameByInstID(ctx, ProcessInstanceID)
+func (e *MyEvent) OnRevoke(ctx context.Context, instID int, revokeUserID string) error {
+	processName, err := e.getProcessNameByInstID(ctx, instID)
 	if err != nil {
 		return err
 	}
-	slog.Info("发起撤销", "流程", processName, "用户", RevokeUserID)
+	slog.Info("发起撤销", "流程", processName, "用户", revokeUserID)
 	return nil
 }
 
